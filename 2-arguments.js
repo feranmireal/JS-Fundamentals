da185f199f1f00dbf30fd6ca750ffdcfc555aaf2
Adesina <adesinaferanmi46@gmail.com>
2025-07-13 16:14:31 +0100

    Fix typo: process.argv instead of ProcessingInstruction.argv

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,4 +1,4 @@
-const args = ProcessingInstruction.argv.length - 2;
+const args = process.argv.length - 2;
 if (args === 0) {console.log('No argument');}
 else if (args === 1) {console.log('Argument found');}
 else {console.log('Arguments found');}
